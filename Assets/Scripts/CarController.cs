e83501dbfe63d33f90b6040890a36229bf393a83
samir2901 <thunder.cse2018@gmail.com>
2020-08-16 22:17:06 +0530

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D frontTireRb, backTireRb, carRb;
+    public float speed = 15f;
+    public float carTorque = 10f;
+    float horizontalInput;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");        
+    }
+    private void FixedUpdate()
+    {
+        backTireRb.AddTorque(-horizontalInput * speed * Time.fixedDeltaTime);
+        backTireRb.AddTorque(-horizontalInput * speed * Time.fixedDeltaTime);
+        carRb.AddTorque(-horizontalInput * carTorque * Time.fixedDeltaTime);
+    }
+}
